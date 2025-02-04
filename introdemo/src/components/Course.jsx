7e47db72173a9b0dd5263e1b82a39a485772f96c
Leimez <leevikempas@gmail.com>
2025-02-04 10:05:57 +0200

    2.4

--- introdemo/src/components/Course.jsx
+++ introdemo/src/components/Course.jsx
@@ -2,10 +2,7 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
-  const totalExercises = course.parts.reduce((sum, part) => {
-    console.log("Summing exercises:", sum, "+", part.exercises);
-    return sum + part.exercises;
-  }, 0); 
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
   return (
     <div>
